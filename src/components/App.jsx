31b92c13a1a0d664cdcbdbc77203067248811a07
nsdq12345 <yeungjian@gmail.com>
2019-07-30 12:28:40 -0700

    added handling of VideoPlayer null values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,53 @@ import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 
-var App = (props) => (
-  <div>
-    {console.log(props)}
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={(this.props.video ? this.props.video : {id: {videoId: 'BLANK'}, snippet: {thumbnails: {default: {url: "BLANK"}}, description: "BLANK", title: "BLANK"}})}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.props.videos}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={props.video}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={props.videos}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
 
+// var App = (props) => (
+// <div>
+//   {console.log(props)}
+//   <nav className="navbar">
+//     <div className="col-md-6 offset-md-3">
+//       <Search />
+//     </div>
+//   </nav>
+//   <div className="row">
+//     <div className="col-md-7">
+//       <VideoPlayer video={props.video}/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={(props.videos ? props.videos : [])}/>
+//     </div>
+//   </div>
+// </div>
+// );
