20903b54f6fd2e3c4b6de56f2dc9c859d08f28c7
nsdq12345 <yeungjian@gmail.com>
2019-07-29 20:57:33 -0700

    videoList can dynamically resize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-var App = () => (
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+
+var App = (props) => (
   <div>
+    {console.log(props)}
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer />
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={props.videos}/>
       </div>
     </div>
   </div>
@@ -19,3 +24,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
